ca7190f452225af6757e182cee55b85fd085d04c
eamonzzz <eamon.zhang@trustasia.com>
2022-08-31 00:26:27 +0800

    chore(project): add 添加 springboot-netty-simple 项目

---
+++ netty/springboot-netty-simple/src/main/java/com/eamonzzz/netty/simple/SpringbootNettySimpleApplication.java
@@ -0,0 +1,19 @@
+package com.eamonzzz.netty.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author eamonzzz
+ * @date 2022-08-31 00:24:05
+ **/
+@SpringBootApplication
+public class SpringbootNettySimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNettySimpleApplication.class, args);
+    }
+
+}
---
+++ netty/springboot-netty-simple/src/test/java/com/eamonzzz/netty/simple/SpringbootNettySimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eamonzzz.netty.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNettySimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
